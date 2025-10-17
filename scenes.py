94f4d48d325cef35a2ca930591d0c449b540494f
oliver earith <100880578+lavamonster09@users.noreply.github.com>
2025-10-17 19:21:38 +0100

    added xp

--- scenes.py
+++ scenes.py
@@ -4,6 +4,7 @@ import pygame
 from player import Player
 from enemy import Enemy
 from weapons import MeleeWeapon
+from xp import Xp
 from ui import * 
 import random
 
@@ -39,7 +40,8 @@ class ExampleScene(Scene):
         enemy = Enemy(self.app, self.player)
         rnd_dir = random.randrange(0, 360)
         enemy.pos = self.player.pos + pygame.Vector2(1,0).rotate(rnd_dir) * 900
-
-
-        self.add_entity(enemy)
+        xp = Xp(self.app, self.player)
+        xp.pos = self.player.pos + pygame.Vector2(1,0).rotate(rnd_dir) * 900
+        #self.add_entity(enemy)
+        self.add_entity(xp)
     
--- xp.py
+++ xp.py
@@ -1,6 +1,17 @@
 import pygame 
 from entity import Entity
 
-class xp(Entity):
-    def __init__(self, app):
+class Xp(Entity):
+    def __init__(self, app, player):
         super().__init__(app, "xp")
+        self.player = player
+
+    def update(self):
+        if self.pos.distance_to(self.player.pos) <= 32:
+            self.attributes["visible"] = False
+            self.player.xp += 10
+            self.app.get_current_scene().entities.remove(self)
+        elif self.pos.distance_to(self.player.pos) <= 128:
+            self.pos = self.pos.move_towards(self.player.pos, 5)
+        super().update() 
+
