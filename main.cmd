f972b5d4bedbd1e8eef6a878618c86ca117898bf
oliver earith <100880578+lavamonster09@users.noreply.github.com>
2025-10-19 18:13:40 +0100

    release

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\python313;%PATH%
+set PYTHONHOME=C:\Python313
+set NUITKA_PYTHONPATH=C:\Users\oliver\Documents\code\Game-Jam;C:\Python313\DLLs;C:\Python313\Lib;C:\Python313;C:\Users\oliver\AppData\Roaming\Python\Python313\site-packages;C:\Python313\Lib\site-packages
+"%~dp0main.exe" %*
